9aea3f618a8302473fbd5ec07e794e47b950fc15
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2018-09-12 18:31:10 -0400

    fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const morgan = require('morgan')
+//const morgan = require('morgan')
 const compression = require('compression')
 const session = require('express-session')
 const passport = require('passport')
@@ -42,7 +42,7 @@ passport.deserializeUser(async (id, done) => {
 
 const createApp = () => {
   // logging middleware
-  app.use(morgan('dev'))
+  //app.use(morgan('dev'))
 
   // body parsing middleware
   app.use(express.json())
